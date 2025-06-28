54af8741c098899645938c8cf91d45469862ea4d
KhanhPoi <khanhlcce181175@fpt.edu.vn>
2025-06-28 22:43:43 +0700

    Cập nhật sign-in.html với Google Sign-In API

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,4 +1,4 @@
-// filepath: d:\HOC\Ki 7\EXE101\MVP\assets\js\sign-in.js
+// Xử lý show/hide password
 const passwordInput = document.getElementById('password');
 const toggleBtn = document.getElementById('toggle-password');
 const eyeIcon = document.getElementById('eye-icon');
@@ -6,13 +6,37 @@ const eyeIcon = document.getElementById('eye-icon');
 toggleBtn.addEventListener('click', () => {
   const isPassword = passwordInput.type === 'password';
   passwordInput.type = isPassword ? 'text' : 'password';
-
   toggleBtn.title = isPassword ? 'Hide password' : 'Show password';
   toggleBtn.ariaLabel = toggleBtn.title;
-
   eyeIcon.textContent = isPassword ? 'visibility_off' : 'visibility';
 });
-
 function showAlert() {
   alert('Xin chào từ JavaScript!');
+}
+
+// Xử lý Google Sign-In nút mặc định
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: "454832286617-jcd3l6ts4hp6ac0qrk5og36eag6glila.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("g_id_signin"),
+    { theme: "outline", size: "large" }
+  );
+};
+
+function handleCredentialResponse(response) {
+  const user = parseJwt(response.credential);
+  window.location.href = "home-page.html";;
+  // window.location.href = "dashboard.html";
+}
+
+function parseJwt(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload);
 }
